a8825b2f3b4927558be4183da7eacddd133a2e3a
Kousuke Fujii <fujii@sharecoto.co.jp>
2018-06-14 16:09:00 +0900

    not found view

---
+++ src/components/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="notfound container">
+    <header>
+      <h1>Not Found.</h1>
+    </header>
+  </div>
+</template>
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Top from './components/Top.vue';
 import Detail from './components/Detail.vue';
+import NotFound from './components/NotFound.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {path: '/', component: Top},
   {path: '/detail/:area/:service/:id', name: 'detail', component: Detail},
+  {path: '*', component: NotFound},
 ];
 
 export default new VueRouter({
